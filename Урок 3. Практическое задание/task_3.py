e25f346a8e48466e9b135974fcf444f70a789129
nut4nat <99830546+nut4nat@users.noreply.github.com>
2023-04-12 20:41:10 +0300

    Update task_3.py
    добавила 2й способ

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -10,4 +10,13 @@ def my_func(*nums):
     s_tuple = sorted(nums)
     return s_tuple[len(s_tuple)-1]*s_tuple[len(s_tuple)-2]
 
-print(my_func(1,7,3))
+def my_func2(*nums):
+    my_list = list(nums)
+    for i in range(len(my_list)-1):
+        for j in range(len(my_list)-i-1):
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+    return my_list[len(my_list)-1] * my_list[len(my_list)-2]
+
+print(my_func(1, 7, 3))
+print(my_func2(1, 7, 3))
