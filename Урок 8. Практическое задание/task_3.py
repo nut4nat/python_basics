11fef11175b43f1cbfccc07da3947e96ec5a4572
nut4nat <99830546+nut4nat@users.noreply.github.com>
2023-05-06 15:31:26 +0300

    Create task_3.py

---
+++ Урок 8. Практическое задание/task_3.py
@@ -0,0 +1,30 @@
+"""
+Задание 3.
+
+Создайте собственный класс-исключение,
+который должен проверять содержимое списка на наличие только чисел.
+
+Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять
+список только числами.
+
+Класс-исключение должен контролировать типы данных элементов списка.
+"""
+
+
+class NotNumError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+lst = []
+
+while True:
+    try:
+        a = input('Input number for the List: ')
+        if not a.isdigit():
+            raise NotNumError('it is not number! Try again to input number!')
+    except NotNumError as err:
+        print(err)
+    else:
+        lst.append(int(a))
+        print(f'Current List: {lst}')
